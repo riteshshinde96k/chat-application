33a9097a262d7deb212014f3691bac1a0646f07a
ritesh shinde <riteshshinde4876@gmail.com>
2026-06-21 17:37:09 +0530

    authenticated & messagecontroller

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
 import userRoute from './routes/userRoute.js';
+import messageRoute from './routes/messageRouter.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -11,10 +12,12 @@ const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json()); 
-app.use(cookieParser());   
+app.use(cookieParser()); 
+
 
 // Routes
 app.use('/api/v1/user',userRoute);
+app.use('/api/v1/message', messageRoute);
 
 app.listen(port, () => {
     connectDB();
