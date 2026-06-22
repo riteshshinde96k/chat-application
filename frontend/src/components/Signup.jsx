37f40d1f5c418d1198df756ca68868bbd6f6a05d
ritesh shinde <riteshshinde4876@gmail.com>
2026-06-22 15:18:01 +0530

    front-end

---
+++ frontend/src/components/Signup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <>
+      <div className="">
+        <h1 className="text-3xl font-bold text-center text-gray-300 ">Signup</h1>
+      </div>
+    </>
+  );
+}
+
+export default Signup;
