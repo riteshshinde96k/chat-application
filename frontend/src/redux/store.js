d3a199ab8b7989a45aaef5d1fdafd74465a534c3
ritesh shinde <riteshshinde4876@gmail.com>
2026-06-24 18:13:58 +0530

    Login

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,9 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import userReducer from "./userSlice.js";
+
+const store = configureStore({
+    reducer:{
+        user:userReducer
+    }
+});
+export default store;
